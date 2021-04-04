47a140bb6d1a070d0657084170fd07f140221bf7
Pratik Shahi <shahipr0@gmail.com>
2021-04-04 08:58:56 +0530

    added pages and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Route, Switch } from "react-router-dom"; //Route is used to define differnt paths we want to listen to and which components should be loaded for different pages
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
+//EXACT only if full path mathes
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favourites">
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
