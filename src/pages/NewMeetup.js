b890e1c1e6895840e704036665783f5ef129bb2e
Pratik Shahi <shahipr0@gmail.com>
2021-04-04 17:57:32 +0530

    can add new meetup to firebase api and its reltime DB

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,16 @@
 import NewMeetForm from "../components/meetups/NewMeetForm";
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-519f0-default-rtdb.firebaseio.com/meetups.json",
+      { method: "POST", body: JSON.stringify(meetupData) }
+    );
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetForm />
+      <NewMeetForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
